c29f1c219a6d99d0a66488af8fb0a08309bdb6c0
yatharth-1504 <bs20b039@smail.iitm.ac.in>
2022-07-04 13:15:24 +0530

    added auth component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,27 +1,42 @@
 <template>
   <div>
-    <ScanComp />
+    <AuthComp v-if="!gotoScan" @submited="setScanner" />
+    <ScanComp v-if="gotoScan" />
   </div>
-</template>
-
-<script>
+</template >
+    
+    <script>
+import AuthComp from './components/auth/AuthComp.vue'
 import ScanComp from './components/scan/ScanComp.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      gotoScan: false
+    }
+  },
+  methods: {
+    setScanner() {
+      this.gotoScan = true;
+      console.log(this.gotoScan);
+    }
+  },
   components: {
     ScanComp,
-  }
+    AuthComp,
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    
+    <style>
+    #app {
+      font-family: Avenir, Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      margin-top: 60px;
+    }
+    </style>
+    
