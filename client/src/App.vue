ba928bf3cc0eced495d9425f0906533e0dbae22b
yatharth-1504 <bs20b039@smail.iitm.ac.in>
2022-12-03 18:45:47 +0530

    removed auth components

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,11 @@
 <template>
   <div>
-    <AuthComp v-if="!auth" @submited="setMount" />
     <MountComp v-if="gotoMount" @scan="setScanner" />
     <ScanComp v-if="gotoScan" @scaned="setMount" />
   </div>
 </template >
     
 <script>
-import AuthComp from './components/auth/AuthComp.vue'
 import ScanComp from './components/scan/ScanComp.vue'
 import MountComp from './components/mount/MountComp.vue'
 
@@ -16,8 +14,7 @@ export default {
   data() {
     return {
       gotoScan: false,
-      auth: false,
-      gotoMount: false,
+      gotoMount: true,
     }
   },
   methods: {
@@ -28,27 +25,25 @@ export default {
     setMount() {
       this.gotoMount = true
       this.gotoScan = false;
-      this.auth = true;
     }
   },
   components: {
     ScanComp,
-    AuthComp,
     MountComp
   },
 }
 </script>
     
-    <style>
-    #app {
-      font-family: Avenir, Helvetica, Arial, sans-serif;
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
-      text-align: center;
-      letter-spacing: 1px;
-      font-weight: bold;
-      color: #13232b;
-      margin-top: 60px;
-    }
-    </style>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #13232b;
+  margin-top: 60px;
+}
+</style>
     
