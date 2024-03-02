4766fe752e26526dd7d81cbaa67ca2225101cf94
Prakash <127179011+ProPrak01@users.noreply.github.com>
2024-03-02 19:38:07 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -22,31 +22,34 @@ app.use(express.static(path.join(__dirname, "../client/public")));
 const port = process.env.PORT;
 const server = app.listen(port, () => {
     console.log(`Express server is running on port ${port}`);
-});
+    // Initialize WebSocket server
+    console.log("server ws working 1");
+    const wss = new WebSocket.Server({ server });
+    console.log("server ws working 2");
+    wss.on('connection', (ws) => {
+        console.log("server ws working");
+        ws.on('message', async (message) => {
+            const data = JSON.parse(message);
+            console.log(message);
 
-// Initialize WebSocket server
-console.log("server ws working 1");
-const wss = new WebSocket.Server({ server });
-console.log("server ws working 2");
-wss.on('connection', (ws) => {
-    console.log("server ws working");
-    ws.on('message', async (message) => {
-        const data = JSON.parse(message);
+            const patientId = data.patientId;
 
-        const patientId = data.patientId;
+            try {
+                const patientData = await xrayController.getPatientData({ params: { patientId } });
 
-        try {
-            const patientData = await xrayController.getPatientData({ params: { patientId } });
-
-            ws.send(JSON.stringify(patientData));
-        } catch (error) {
-            console.error('Error retrieving patient data:', error);
-            ws.send(JSON.stringify({ error: 'Error retrieving patient data' }));
-        }
+                ws.send(JSON.stringify(patientData));
+            } catch (error) {
+                console.error('Error retrieving patient data:', error);
+                ws.send(JSON.stringify({ error: 'Error retrieving patient data' }));
+            }
+        });
     });
-});
 
-// Handle WebSocket server errors
-wss.on('error', (error) => {
-    console.error('WebSocket server error:', error);
-});
+    // Handle WebSocket server errors
+    wss.on('error', (error) => {
+        console.error('WebSocket server error:', error);
+    });
+}
+);
+
+
